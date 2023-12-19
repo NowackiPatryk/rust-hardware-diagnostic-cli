22aeac83a39c3dbd4d81521ae4494e6a89db710b
Patryk Nowacki prv <patryknowackiprv@MacBook-Air-Patryk.local>
2023-12-19 22:03:35 +0100

    feat: min max temps

--- src/cli/handlers/mod.rs
+++ src/cli/handlers/mod.rs
@@ -1,15 +1,46 @@
 mod utils;
-use crate::sysinfo::{get_cpu_info, get_components_temp, ComponentTemperature, CpuInfo};
+use std::collections::HashMap;
 
-fn format_temperature_message(temp: ComponentTemperature) -> String {
-  format!("{} : {:.2}°C", temp.label, temp.celsius_temp)
+use crate::sysinfo::{get_cpu_info, get_components_temp, CpuInfo};
+
+#[derive(Clone)]
+struct MinMaxComponentTemps {
+  label: String,
+  current_temp: f32,
+  max_temp: f32,
+  min_temp: f32,
+}
+
+fn format_temperature_message(temp: MinMaxComponentTemps) -> String {
+  format!("{}: \n Current: {:.2}°C \n Min: {:.2}°C \n Max: {:.2}°C \n\n", temp.label, temp.current_temp, temp.min_temp, temp.max_temp)
 }
 
 fn format_cpu_info_message(cpu_info: CpuInfo) -> String {
   format!("#{} - Frequency: {}HZ, Usage: {:.1}%", cpu_info.number, cpu_info.frequency, cpu_info.usage)
 }
 
+fn get_initial_min_max_temps() -> HashMap<String, MinMaxComponentTemps> {
+  let mut min_max_temps:  HashMap<String, MinMaxComponentTemps> = HashMap::new();
+
+  let result = get_components_temp();
+
+  for temp in result {
+    let label = temp.label.clone();
+
+    min_max_temps.insert(temp.label, MinMaxComponentTemps {
+      label: label,
+      current_temp: temp.celsius_temp.clone(),
+      max_temp: temp.celsius_temp.clone(),
+      min_temp: temp.celsius_temp.clone(),
+    });
+  }
+
+  min_max_temps
+}
+
 pub fn display_components_temperatures_live() {
+  let mut min_max_temps = get_initial_min_max_temps();
+
   loop {
     utils::clear_screen();
     utils::wait(500);
@@ -17,8 +48,20 @@ pub fn display_components_temperatures_live() {
     let result = get_components_temp();
 
     for temp in result {
-      println!("{}", format_temperature_message(temp));
-    }
+      let temps = min_max_temps.get_mut(temp.label.as_str()).unwrap();
+
+      temps.current_temp = temp.celsius_temp;
+
+      if temp.celsius_temp > temps.max_temp {
+        temps.max_temp = temp.celsius_temp;
+      }
+
+      if temp.celsius_temp < temps.min_temp {
+        temps.min_temp = temp.celsius_temp;
+      }
+
+      println!("{}", format_temperature_message(temps.clone()));
+    };
   }
 }
 
