bcd575daf85c518def96c995ea6fd7a882f403f9
Patryk Nowacki prv <patryknowackiprv@MacBook-Air-Patryk.local>
2023-12-20 19:20:27 +0100

    refactor: better structure

--- src/sysinfo/mod.rs
+++ src/sysinfo/mod.rs
@@ -1,35 +1,25 @@
 use sysinfo::{System, SystemExt, ComponentExt, Component, CpuExt, Cpu};
+pub mod structs;
 
-pub struct ComponentTemperature {
-  pub label: String,
-  pub celsius_temp: f32,
-}
-
-pub struct CpuInfo {
-  pub number: usize,
-  pub frequency: u64,
-  pub usage: f32,
-}
-
-pub fn get_components_temp() -> Vec<ComponentTemperature> {
+pub fn get_components_temp() -> Vec<structs::ComponentTemperature> {
   let mut sys = System::new_all();
   sys.refresh_components();
 
   extract_components_temp(sys.components())
 }
 
-pub fn get_cpu_info() -> Vec<CpuInfo> {
+pub fn get_cpu_info() -> Vec<structs::CpuInfo> {
   let mut sys = System::new_all();
   sys.refresh_cpu();
 
   extract_cpu_info(sys.cpus())
 }
 
-fn extract_components_temp(components: &[Component]) -> Vec<ComponentTemperature> {
-  let mut temps: Vec<ComponentTemperature> = Vec::new();
+fn extract_components_temp(components: &[Component]) -> Vec<structs::ComponentTemperature> {
+  let mut temps: Vec<structs::ComponentTemperature> = Vec::new();
 
   for component in components {
-    temps.push(ComponentTemperature{
+    temps.push(structs::ComponentTemperature{
       label: component.label().to_string(),
       celsius_temp: component.temperature(),
     })
@@ -38,11 +28,11 @@ fn extract_components_temp(components: &[Component]) -> Vec<ComponentTemperature
   temps
 }
 
-fn extract_cpu_info(cpus: &[Cpu]) -> Vec<CpuInfo> {
-  let mut cpu_info: Vec<CpuInfo> = Vec::new();
+fn extract_cpu_info(cpus: &[Cpu]) -> Vec<structs::CpuInfo> {
+  let mut cpu_info: Vec<structs::CpuInfo> = Vec::new();
 
   for (index, cpu) in cpus.iter().enumerate() {
-    cpu_info.push(CpuInfo { number: index + 1, frequency: cpu.frequency(), usage: cpu.cpu_usage() })
+    cpu_info.push(structs::CpuInfo { number: index + 1, frequency: cpu.frequency(), usage: cpu.cpu_usage() })
   };
 
   cpu_info
