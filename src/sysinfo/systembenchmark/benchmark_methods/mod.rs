bc4781eee1c64ed0f2ef79d38afeeb675bbfe0a4
Patryk Nowacki prv <patryknowackiprv@MacBook-Air-Patryk.local>
2023-12-27 10:18:43 +0100

    refactor: cpu benchmark

---
+++ src/sysinfo/systembenchmark/benchmark_methods/mod.rs
@@ -0,0 +1,4 @@
+pub fn square_benchmark() {
+    let number_to_square: f32 = 123456.789;
+    let _ = number_to_square.sqrt();
+}
--- src/sysinfo/systembenchmark/mod.rs
+++ src/sysinfo/systembenchmark/mod.rs
@@ -1,23 +1,63 @@
-use std::time::SystemTime;
+use std::{
+    thread::{self, available_parallelism, JoinHandle},
+    time::SystemTime,
+};
+mod benchmark_methods;
 
-pub fn benchmark_cpu() -> u128 {
+pub fn process_benchmark() -> u128 {
+    const BENCHMARKS_NUMBER: u128 = 100;
+    let mut final_score: u128 = 0;
+
+    let threads = setup_benchmark_threads(BENCHMARKS_NUMBER);
+    let threads_number = threads.len() as u64;
+
+    for thread in threads {
+        let result = thread.join().unwrap();
+        final_score += result / threads_number as u128;
+    }
+
+    final_score
+}
+
+fn setup_benchmark_threads(benchmarks_number: u128) -> Vec<JoinHandle<u128>> {
+    let threads_number = (available_parallelism().unwrap().get()) as u64;
+    let mut threads = vec![];
+
+    for _ in 1..threads_number {
+        let handle = thread::spawn(move || handle_thread_benchmark(benchmarks_number));
+
+        threads.push(handle);
+    }
+
+    threads
+}
+
+fn handle_thread_benchmark(benchmarks_number: u128) -> u128 {
+    let mut final_score: u128 = 0;
+
+    for _ in 1..benchmarks_number {
+        final_score += benchmark_cpu() / benchmarks_number;
+    }
+
+    final_score
+}
+
+fn benchmark_cpu() -> u128 {
     const FULL_SCORE: u128 = 1000;
     const EPOCHS: i32 = 10000000;
 
-    let mut number_to_square: f32 = 123456.789;
-    let mut current_epoch = 1;
-
     let benchmark_start = SystemTime::now();
-    while current_epoch <= EPOCHS {
-        number_to_square = number_to_square.sqrt();
-        current_epoch += 1;
-    }
-    let benchmark_end = SystemTime::now();
 
-    let duration = benchmark_end
-        .duration_since(benchmark_start)
-        .unwrap()
-        .as_millis();
+    for _ in 1..EPOCHS {
+        benchmark_methods::square_benchmark();
+    }
+
+    let benchmark_end = SystemTime::now();
+    let duration = calculate_duration(benchmark_start, benchmark_end);
 
     FULL_SCORE - duration
+}
+
+fn calculate_duration(start: SystemTime, end: SystemTime) -> u128 {
+    end.duration_since(start).unwrap().as_millis()
 }
